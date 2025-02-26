fe56a7b4ae508a46c8101ce02a0c7e7e7717d250
Cindy Wang <cindy.wang1@kellogg.northwestern.edu>
2025-02-26 02:17:38 +0000

    1

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # get("/", { :controller => "articles", :action => "index" })
+get("/", { :controller => "articles", :action => "index" })
+# Routes for Places
+get("/places", { controller: "places", action: "index" })       # List all places
+get("/places/new", { controller: "places", action: "new" })     # New place form
+post("/places", { controller: "places", action: "create" })     # Create a place
+get("/places/:id", { controller: "places", action: "show" })    # Show a place
 end
